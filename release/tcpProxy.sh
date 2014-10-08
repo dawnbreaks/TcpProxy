27b30e464994d0a23f101dc4de9cb77a97988faa
lubinwang <308181687@qq.com>
2014-10-08 14:44:58 +0800

    Update tcpProxy.sh

--- release/tcpProxy.sh
+++ release/tcpProxy.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-pid=`ps -ef|grep java|grep tcpproxy|awk '{print $2}'`
+currentDir=`readlink -m  $(dirname $0)`
+targetDir=$currentDir/../target
+jarFileName="TcpProxy-0.0.1-SNAPSHOT.jar"
+
+pid=`ps -ef|grep java|grep TcpProxyServer|awk '{print $2}'`
 if [ "x$pid" != "x" ];
 then
-    echo  "killing service process." 
+    echo  "Killing service process." 
     kill  -9 $pid
     sleep  4
 fi
 
-echo "starting service....."
-nohup java -cp ./dependency/*:./*:./config   com.lubin.tcpproxy.TcpProxyServer  2>&1 >> ./tcpProxy.log  &
+
+if [ -f $targetDir/$jarFileName ];
+then
+  rm -f $currentDir/$jarFileName
+  cp -f $targetDir/$jarFileName $targetDir
+fi
+
+echo "Starting service....."
+nohup java -cp $currentDir/dependency/*:$currentDir/*:$currentDir/config   com.lubin.tcpproxy.TcpProxyServer  2>&1 >> $currentDir/tcpProxy.log  &
+echo "Done....."
